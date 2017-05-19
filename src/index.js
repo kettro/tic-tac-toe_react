93c0d7836df179110122de7b7d4f0a86b67a904b
kettro <rkettleson@gmail.com>
2017-05-19 09:02:57 -0700

    Going through the tutorial...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+class Square extends React.Component {
+  render() {
+    return (
+      // Passing the onClick listener down via the props
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+class Board extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i){
+    // copying the existing array
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i){
+    return (
+      <Square
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render(){
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/**/}</div>
+          <ol>{/**/}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <Game />,
   document.getElementById('root')
 );
